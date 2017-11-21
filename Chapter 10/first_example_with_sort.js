91d36a4943d84b6a3b70fe9327c9311722f5bd8e
Rafael Alejandro <lx.garduno@gmail.com>
2017-11-21 17:09:59 -0600

    Create first_example_with_sort.js

---
+++ Chapter 10/first_example_with_sort.js
@@ -0,0 +1,4 @@
+// This is the first example with the sort method
+var initial_array = new Array("A", "C", "B", "G", "Z");
+initial_array.sort();
+// sorts the array so that my initial_array = [ "A","B","C","G","Z" ]
