0b66178534a9bd9a2b41bab48011785358bf52b7
Rafael Alejandro <lx.garduno@gmail.com>
2017-11-21 17:04:53 -0600

    Create Filter_example.js

---
+++ Chapter 10/Filter_example.js
@@ -0,0 +1,5 @@
+// This returns the numbers different from zero
+[1,0,2,0,3,0,4,0,5,0,6,0,7,0,8,0,9].filter(function(x){ return x != 0 })
+
+//The output using the filter method from the initial array is :
+> {9} [1,2,3,4,5,6,7,8,9]
