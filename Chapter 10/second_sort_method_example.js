f9cac63ca673f1c248c429f433d670a5399d1bd4
Rafael Alejandro <lx.garduno@gmail.com>
2017-11-21 17:10:37 -0600

    Create second_sort_method_example.js

---
+++ Chapter 10/second_sort_method_example.js
@@ -0,0 +1,6 @@
+["Java", "PHP", "C#", "JavaScript", "Ruby", "Perl", "HTML", "Go"].sort();
+// => ["C#", "Go", "HTML", "Java", "JavaScript", "PHP", "Perl", "Ruby"]
+ 
+ 
+[15, 41, 1, 75, 35, 100, 123, 1000].sort();
+// => [1, 100, 1000, 123, 15, 35, 41, 75]
