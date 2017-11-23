3d531baf0d9100794a42911b2f41dc0a1171f2e7
Rafael Alejandro <lx.garduno@gmail.com>
2017-11-22 18:29:35 -0600

    Create first_way_examples.js

---
+++ Chapter 11/first_way_examples.js
@@ -0,0 +1,11 @@
+//This file contains examples of the first way to initialize the date constructor
+//Given the year, month, day, hour and minutes
+
+new Date(2002,1,30,14,55);
+// => Sat Mar 02 2002 14:55:00 GMT-0600 (CST)
+
+new Date(2004,5,17,22,55);
+// => Thu Jun 17 2004 22:55:00 GMT-0500 (CDT)
+
+new Date(2017,10,22);
+// => Wed Nov 22 2017 00:00:00 GMT-0600 (CST)
