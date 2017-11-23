2a25e07857fbf985b90b3991d8f6208db27ac49d
Rafael Alejandro <lx.garduno@gmail.com>
2017-11-22 19:03:01 -0600

    Create fourth_way_examples.js

---
+++ Chapter 11/fourth_way_examples.js
@@ -0,0 +1,10 @@
+//This file contains examples of the fourth way to initialize the date constructor
+
+new Date()
+// => Wed Nov 22 2017 19:00:26 GMT-0600 (CST)
+
+new Date(Date.now())
+// => Wed Nov 22 2017 19:00:26 GMT-0600 (CST)
+
+Date.now()
+// => 1511398885223
