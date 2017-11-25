cebc7ce0576f0ab6e5d564f05ae660d4192f980a
Rafael Alejandro <lx.garduno@gmail.com>
2017-11-24 22:19:20 -0600

    Create universal_time_sintax.js

---
+++ Chapter 11/universal_time_sintax.js
@@ -0,0 +1,6 @@
+//This is the universal time sintax
+
+Date.prototype.(getUTC-setUTC)<method>();
+
+//the getters return a method, according to universal time
+//the setters set a method, according to universal time.
