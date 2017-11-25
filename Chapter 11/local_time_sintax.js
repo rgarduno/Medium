736665b6766381a30577407461e336b86c8abb8f
Rafael Alejandro <lx.garduno@gmail.com>
2017-11-24 22:18:54 -0600

    Create local_time_sintax.js

---
+++ Chapter 11/local_time_sintax.js
@@ -0,0 +1,6 @@
+//This is the local time sintax
+
+Date.prototype.(get-set)<method>();
+
+//the getters return a method, according to local time
+//the setters set a method, according to local time.
