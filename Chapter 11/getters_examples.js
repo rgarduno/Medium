9ef66c39197c6fa6ae336ef446c5cf87f6a2a82d
Rafael Alejandro <lx.garduno@gmail.com>
2017-11-24 22:43:59 -0600

    Create getters_examples.js

---
+++ Chapter 11/getters_examples.js
@@ -0,0 +1,37 @@
+//Here are the example for the getters
+
+//let's use a date 
+var example_date = new Date();
+// => Fri Nov 24 2017 22:17:37 GMT-0600 (CST)
+
+example_date.getDate()
+// Returns the day of the month (1-31) for the specified date according to local time.
+// => 24
+
+example_date.getDay()
+// Returns the day of the week (0-6) for the specified date according to local time.
+// => 5
+
+example_date.getFullYear()
+// Returns the year (4 digits for 4-digit years) of the specified date according to local time.
+// => 2017
+
+example_date.getHours()
+// Returns the hour (0-23) in the specified date according to local time.
+// => 22
+
+example_date.getMilliseconds()
+// Returns the milliseconds (0-999) in the specified date according to local time.
+// => 542
+
+example_date.getMinutes()
+// Returns the minutes (0-59) in the specified date according to local time.
+// => 17
+
+example_date.getMonth()
+// Returns the month (0-11) in the specified date according to local time.
+// => 10
+
+example_date.getSeconds()
+// Returns the seconds (0-59) in the specified date according to local time.
+// => 37
